c16dd0cc3eaf15fd19cfe4a7612d055fbcac1d26
Quentin Phan <92746951+quentox@users.noreply.github.com>
2023-12-04 15:14:11 +0100

    ajout proxy

---
+++ proxy.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from Manager import QueueClient
+
+
+class Proxy(BaseHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        self.client = QueueClient()
+        super().__init__(*args, **kwargs)
+
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
+        t = self.client.results.get()
+        self.wfile.write(bytes(t.to_json(), "utf-8"))
+
+
+def run(server_class=HTTPServer, handler_class=Proxy):
+    server_address = ("", 8000)
+    httpd = server_class(server_address, handler_class)
+    httpd.serve_forever()
+
+
+if __name__ == "__main__":
+    run()
